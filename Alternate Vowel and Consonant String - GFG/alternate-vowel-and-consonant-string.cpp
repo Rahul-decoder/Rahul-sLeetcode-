0dfb8700b0dc024f211961ff0e52e61ac857031c
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2022-12-04 20:59:38 +0530

    Added solution - LeetHub

--- Alternate Vowel and Consonant String - GFG/alternate-vowel-and-consonant-string.cpp
+++ Alternate Vowel and Consonant String - GFG/alternate-vowel-and-consonant-string.cpp
@@ -4,85 +4,90 @@ using namespace std;
 
 
 // } Driver Code Ends
-
- 
 class Solution{
     public:
     string rearrange (string s, int n)
     {
-        string s1="";
-        int i,a[26]={0},j=1;
+        int i,j,a[26]={-1},b[26]={0},count=0;
+        a[0]=0,a[4]=0,a[8]=0,a[14]=0,a[20]=0;
         for(i=0;i<n;i++)
-            a[s[i]-'a']=a[s[i]-'a']+1;
-        int p=a[0]+a[4]+a[8]+a[14]+a[20],sum=0,flag=0;
-        for(i=0;i<26;i++)
         {
-            if(i!=0&&i!=4&&i!=8&&i!=14&&i!=20)
-            sum=sum+a[i];
+            if(s[i]=='a')
+             a[0]++;
+            else if(s[i]=='e')
+             a[4]++;
+            else if(s[i]=='i')
+             a[8]++;
+            else if(s[i]=='o')
+             a[14]++;
+            else if(s[i]=='u')
+             a[20]++;
+            if(s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u')
+            {
+                b[s[i]-'a']++;
+                count++;
+            }
         }
-        if(abs(sum-p)>1)
+        int p=n-count,k=0,flag=0;
+        i=0,j=0;
+        if(abs(count-p)>1)
         return "-1";
         else
         {
-                         while(a[j]<=0)
-                         {
-                           j++;
-                           if(j==4||j==8||j==14||j==20)
-                           j++;
-                         }
-                 if(sum==p)
-                    {
-                         if(((j+'a')<'e'&&a[0]==0)||((j+'a')<'i'&&a[4]==0&&a[0]==0)||((j+'a')<'o'&&a[8]==0&&a[4]==0&&a[0]==0)||((j+'a')<'u'&&a[14]==0&&a[0]==0&&a[4]==0&&a[8]==0))
-                         {
-                             s1.push_back((j+'a'));
-                             a[j]--;
-                         }
-                    }
-                    if(sum>p)
-                     {
-                         s1.push_back((j+'a'));
-                         a[j]--;
-                     }
-                while(s1.size()<n)
+           while(a[i]<=0)
+               i++;
+             while(b[j]<=0)
+               j++;
+            if(count==p)
+            {
+                if(i>j)
                 {
-                    if(a[0]>0)
-                    {
-                    s1.push_back('a');
-                    a[0]--;
-                    }
-                    else if(a[4]>0)
-                    {
-                    s1.push_back('e');
-                    a[4]--;
-                    }
-                    else if(a[8]>0)
-                    {
-                        s1.push_back('i');
-                        a[8]--;
-                    }
-                    else if(a[14]>0)
-                    {
-                        s1.push_back('o');
-                        a[14]--;
-                    }
-                    else if(a[20]>0)
-                    {
-                        s1.push_back('u');
-                        a[20]--;
-                    }
-                    if(s1.size()==n)
-                    break;
-                    while(a[j]<=0)
-                    {
-                        j++;
-                        if(j==4||j==8||j==14||j==20)
-                        j++;
-                    }
-                    s1.push_back((j+'a'));
-                    a[j]--;
+                s[k]=j+'a';
+                b[j]--;
+                flag=1;
                 }
+                else
+                {
+                s[k]=i+'a';
+                a[i]--;
+                }
+                k++;
+            }
+            if(p>count)
+            {
+                s[k]=i+'a';
+                a[i]--;
+                k++;
+            }
+            while(k<n)
+            {
+                while(a[i]<=0)
+                i++;
+                while(b[j]<=0)
+                j++;
+                if(flag!=1)
+                {
+                s[k]=j+'a';
+                b[j]--;
+                k++;
+                }
+                if(k==n)
+                break;
+                s[k]=i+'a';
+                a[i]--;
+                k++;
+                if(k==n)
+                break;
+                if(flag==1)
+                {
+                s[k]=j+'a';
+                b[j]--;
+                k++;
+                }
+            }
         }
-        return s1;
+        return s;
+        
     }
 };
 
