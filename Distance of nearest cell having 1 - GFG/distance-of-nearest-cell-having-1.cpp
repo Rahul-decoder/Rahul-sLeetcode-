a1e93a047084182c6ac2ffa2f3fe40c2eb456e54
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-02-16 19:43:17 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -9,11 +9,12 @@ class Solution
     //Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-	    int i,j;
+	    int i,j,ans=0;
 	    int n=grid.size(),m=grid[0].size();
-	    queue<pair<pair<int,int>,int>> q;
+	    queue<vector<pair<int,int>>> q;
 	    vector<vector<int>> vis(n,vector<int>(m,0));
 	    vector<vector<int>> dis(n,vector<int> (m,0));
+	     vector<pair<int,int>> v1;
 	    for(i=0;i<n;i++)
 	    {
 	        for(j=0;j<m;j++)
@@ -21,28 +22,38 @@ class Solution
 	            if(grid[i][j]==1)
 	            {
 	                vis[i][j]=1;
-                    q.push({{i,j},0});
+                    v1.push_back({i,j});
 	            }
 	        }
 	    }
+	    q.push(v1);
+	    int dr[]={1,0,-1,0};
+	    int dc[]={0,-1,0,1};
 	    while(!q.empty())
 	    {
-	        int r=q.front().first.first;
-	        int c=q.front().first.second;
-	        int s=q.front().second;
-	        dis[r][c]=s;
+	        vector<pair<int,int>> v,h;
+	        v=q.front();
 	        q.pop();
-	        int dr[]={-1,0,1,0};
-	        int dc[]={0,1,0,-1};
-	        for(i=0;i<4;i++)
+	        for(i=0;i<v.size();i++)
 	        {
-	            int r1=dr[i]+r;
-	            int c1=dc[i]+c;
-	            if(r1>=0&&r1<n&&c1>=0&&c1<m&&vis[r1][c1]==0)
+	            int r=v[i].first;
+	            int c=v[i].second;
+	            dis[r][c]=ans;
+	            for(j=0;j<4;j++)
 	            {
-	                vis[r1][c1]=1;
-	                q.push({{r1,c1},s+1});
+	                int r1=r+dr[j];
+	                int c1=c+dc[j];
+	                if(r1>=0&&r1<n&&c1>=0&&c1<m&&vis[r1][c1]==0)
+	                {
+	                    vis[r1][c1]=1;
+	                    h.push_back({r1,c1});
+	                }
 	            }
+	        }
+	        if(h.size()>0)
+	        {
+	            ans++;
+	            q.push(h);
 	        }
 	    }
 	    return dis;
