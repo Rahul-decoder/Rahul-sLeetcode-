ba15d446ea434a5766a6bbfd1424e290d81491c9
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-06-09 21:22:08 +0530

    Added solution - LeetHub

--- Partition array to K subsets - GFG/partition-array-to-k-subsets.cpp
+++ Partition array to K subsets - GFG/partition-array-to-k-subsets.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 
 // } Driver Code Ends
-/*You are required to complete this method */
-
 class Solution{
   public:
-    void fun(int a[],int n,vector<int> v,int i,int k,int &flag)
+    bool fun(int a[],int n,vector<int> v,int i,int k)
     {
         if(i==n)
         {
@@ -21,30 +19,29 @@ class Solution{
                     count++;
                 }
                 if(count==k-1)
-                flag=1;
+                return 1;
             }
-           return;
+            return 0;
         }
         for(int j=0;j<v.size();j++)
         {
             v[j]+=a[i];
-            fun(a,n,v,i+1,k,flag);
+            if(fun(a,n,v,i+1,k))
+            return 1;
             v[j]-=a[i];
         }
         if(v.size()<k)
         {
             v.push_back(a[i]);
-            fun(a,n,v,i+1,k,flag);
+            if(fun(a,n,v,i+1,k))
+            return 1;
         }
+        return 0;
     }
     bool isKPartitionPossible(int a[], int n, int k)
     {
          vector<int> v;
-         int i,flag=0;
-         fun(a,n,v,0,k,flag);
-         if(flag==1)
-         return 1;
-         return 0;
+         return fun(a,n,v,0,k);
     }
 };
 
