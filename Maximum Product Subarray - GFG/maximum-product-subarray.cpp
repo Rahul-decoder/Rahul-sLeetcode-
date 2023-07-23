d187ae40bd45c8b73d8a6f7cd348c3cec024ce3e
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-07-23 20:06:25 +0530

    Added solution - LeetHub

--- Maximum Product Subarray - GFG/maximum-product-subarray.cpp
+++ Maximum Product Subarray - GFG/maximum-product-subarray.cpp
@@ -10,76 +10,17 @@ public:
 
 	// Function to find maximum product subarray
 	long long maxProduct(vector<int> arr, int n) {
-	    if(n==1)
-	    return arr[0];
-	    long long int mul=1,maxi=LLONG_MIN,i=0,j=0,k,count=0,flag=1;
-	    for(k=0;k<n-1;k++)
+	    long long int p=1,s=1,i,maxi=LLONG_MIN;
+	    for(i=0;i<n;i++)
 	    {
-	        if(arr[k]<0&&arr[k+1]<0)
-	        flag=0;
+	        if(p==0)
+	        p=1;
+	        if(s==0)
+	        s=1;
+	        p=p*arr[i];
+	        s=s*arr[n-i-1];
+	        maxi=max(maxi,max(p,s));
 	    }
-	    while(j<n)
-	    {
-	        if(arr[j]==0)
-	        {
-	            if(count%2!=0)
-	            {
-	                long long int mul1=mul;
-	                while(1)
-	                {
-	                    mul1=mul1/arr[i];
-	                    if(arr[i]<0)
-	                    break;
-	                    i++;
-	                }
-	                maxi=max(maxi,mul1);
-	                mul1=mul;
-	                k=j-1;
-	                while(1)
-	                {
-	                    mul1=mul1/arr[k];
-	                    if(arr[k]<0)
-	                    break;
-	                    k--;
-	                }
-	                 maxi=max(maxi,mul1);
-	            }
-	            i=j+1;
-	            count=0;
-	        }
-	        if(arr[j]<0)
-	        count++;
-	        if(arr[j]!=0)
-	        mul=mul*arr[j];
-	        else
-	        mul=1;
-	        maxi=max(maxi,mul);
-	        j++;
-	    }
-	    if(count%2!=0)
-	    {
-	        long long int mul1=mul;
-	        while(1)
-	        {
-	            mul1=mul1/arr[i];
-	            if(arr[i]<0)
-	            break;
-	            i++;
-	        }
-	        maxi=max(maxi,mul1);
-	        mul1=mul;
-	        k=j-1;
-	        while(1)
-            {
-                mul1=mul1/arr[k];
-                if(arr[k]<0)
-                break;
-                k--;
-            }
-            maxi=max(maxi,mul1);
-	    }
-	    if(maxi==1&&flag==1)
-	    return 0;
 	    return maxi;
 	}
 };
