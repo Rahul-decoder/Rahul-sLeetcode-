6f5e492a46954f7d982c898e8f8043294f44583a
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-09-18 21:17:28 +0530

    Added solution - LeetHub

---
+++ Power of 2 - GFG/power-of-2.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool isPowerofTwo(long long n){
+        
+        long long int sum=1;
+        while(sum<=n)
+        {
+            if(sum==n)
+            return 1;
+            sum=sum<<1;
+        }
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
