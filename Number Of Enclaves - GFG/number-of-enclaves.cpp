582668c1f8581038b69cf1a1d5782db471e6ca59
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-09-29 00:17:04 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,39 +10,50 @@ using namespace std;
 
 class Solution {
   public:
-  void dfs(vector<vector<int>> &grid,int i,int j,int n,int m)
+  int count=0;
+  void fun(vector<vector<int>>&grid,int i,int j,int n,int m)
   {
-      if(i>=n||j>=m||i<0||j<0||grid[i][j]==0)
+      if(i<0||j<0||i>=n||j>=m||grid[i][j]==0)
       return ;
       grid[i][j]=0;
-      dfs(grid,i+1,j,n,m);
-      dfs(grid,i-1,j,n,m);
-      dfs(grid,i,j-1,n,m);
-      dfs(grid,i,j+1,n,m);
+      count++;
+      fun(grid,i+1,j,n,m);
+      fun(grid,i-1,j,n,m);
+      fun(grid,i,j+1,n,m);
+      fun(grid,i,j-1,n,m);
   }
     int numberOfEnclaves(vector<vector<int>> &grid) {
-       int i,j,n=grid.size(),m=grid[0].size(),count=0;
-       for(i=0;i<n;i++)
-       {
-          if(grid[i][0]==1)
-          dfs(grid,i,0,n,m);
-          if(grid[i][m-1]==1)
-          dfs(grid,i,m-1,n,m);
-       }
-       for(i=0;i<m;i++)
-       {
-          if(grid[0][i]==1)
-          dfs(grid,0,i,n,m);
-          if(grid[n-1][i]==1)
-          dfs(grid,n-1,i,n,m);
-       }
-       for(i=0;i<n;i++)
-       {
-           for(j=0;j<m;j++)
-           if(grid[i][j]==1)
-           count++;
-       }
-       return count;
+         int i,j,n=grid.size(),m=grid[0].size();
+         for(i=0;i<n;i++)
+         {
+            if(grid[i][0]==1)
+            fun(grid,i,0,n,m);
+         }
+         for(i=0;i<n;i++)
+         {
+             if(grid[i][m-1]==1)
+             fun(grid,i,m-1,n,m);
+         }
+         for(i=0;i<m;i++)
+         {
+             if(grid[0][i]==1)
+             fun(grid,0,i,n,m);
+         }
+         for(i=0;i<m;i++)
+         {
+             if(grid[n-1][i]==1)
+             fun(grid,n-1,i,n,m);
+         }
+         count=0;
+         for(i=1;i<n-1;i++)
+         {
+             for(j=1;j<m-1;j++)
+             {
+                 if(grid[i][j]==1)
+                 fun(grid,i,j,n,m);
+             }
+         }
+         return count;
     }
 };
 
