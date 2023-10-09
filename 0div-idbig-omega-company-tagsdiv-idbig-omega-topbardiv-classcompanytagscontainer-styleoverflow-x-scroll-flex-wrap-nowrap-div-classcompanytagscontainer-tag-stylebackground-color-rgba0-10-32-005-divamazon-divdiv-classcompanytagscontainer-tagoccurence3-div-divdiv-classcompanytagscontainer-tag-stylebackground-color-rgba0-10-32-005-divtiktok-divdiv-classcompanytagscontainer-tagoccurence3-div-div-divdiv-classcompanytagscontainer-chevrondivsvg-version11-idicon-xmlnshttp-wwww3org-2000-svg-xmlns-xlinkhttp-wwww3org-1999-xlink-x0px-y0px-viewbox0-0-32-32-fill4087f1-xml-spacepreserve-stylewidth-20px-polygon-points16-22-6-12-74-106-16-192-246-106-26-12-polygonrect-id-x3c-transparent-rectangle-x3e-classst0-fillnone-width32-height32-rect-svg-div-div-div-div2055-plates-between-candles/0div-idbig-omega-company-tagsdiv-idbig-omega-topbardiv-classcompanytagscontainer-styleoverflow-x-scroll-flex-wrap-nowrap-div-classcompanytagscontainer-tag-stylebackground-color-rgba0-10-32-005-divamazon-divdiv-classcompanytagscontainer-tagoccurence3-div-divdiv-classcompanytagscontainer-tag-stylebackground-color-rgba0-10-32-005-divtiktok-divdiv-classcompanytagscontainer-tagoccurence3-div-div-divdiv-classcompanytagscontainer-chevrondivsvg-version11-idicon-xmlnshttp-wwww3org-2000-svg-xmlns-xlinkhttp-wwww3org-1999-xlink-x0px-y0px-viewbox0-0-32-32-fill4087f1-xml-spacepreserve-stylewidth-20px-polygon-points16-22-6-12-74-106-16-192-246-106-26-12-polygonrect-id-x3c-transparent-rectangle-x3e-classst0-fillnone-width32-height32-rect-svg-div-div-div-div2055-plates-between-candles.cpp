bac22a6ec316f983bd06499e24f9ebd6cf0b9680
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-10-09 14:54:08 +0530

    Time: 352 ms (74.04%), Space: 142.8 MB (61.67%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2055-plates-between-candles/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2055-plates-between-candles.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+        int i,n=s.size(),count=0,n1=queries.size();
+        vector<int> v,vs(n),ans;
+        for(i=0;i<n;i++)
+        {
+           if(s[i]=='|')
+               v.push_back(i);
+            else
+                count++;
+            vs[i]=count;
+        }
+        for(i=0;i<n1;i++)
+        {
+            int a=queries[i][0],b=queries[i][1],p=0,q=0;
+            int l=lower_bound(v.begin(),v.end(),a)-v.begin();
+            int u=lower_bound(v.begin(),v.end(),b)-v.begin();
+            if(l<v.size()&&v[l]>b||l==v.size())
+                l=-1;
+            if(u==v.size())
+                u--;
+            if(u>=0&&v[u]>b)
+                u--;
+            if(l>=0&&u>=0)
+            p=v[l],q=v[u];
+            ans.push_back(vs[q]-vs[p]);
+        }
+        return ans;
+    }
+};
