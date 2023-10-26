3d10efdb9eccdc7dbd1ed89cd19fe9eeb60e84ef
Rahul Gupta <110086308+Rahul-decoder@users.noreply.github.com>
2023-10-26 21:45:32 +0530

    Added solution - LeetHub

--- Minimum Operations - GFG/minimum-operations.cpp
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -8,21 +8,21 @@ class Solution
   public:
     int minOperation(int n)
     {
-       int count=0;
-       while(n!=0)
-       {
-           if(n%2==0)
-           {
-               n=n/2;
-               count++;
-           }
-           else
-           {
-               n=n-1;
-               count++;
-           }
-       }
-       return count;
+        int count=0;
+        while(n>0)
+        {
+            if(n%2!=0)
+            {
+                count++;
+                n=n-1;
+            }
+            else
+            {
+                count++;
+                n=n/2;
+            }
+        }
+        return count;
     }
 };
 
